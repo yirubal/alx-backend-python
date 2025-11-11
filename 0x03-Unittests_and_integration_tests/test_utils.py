2d48ff1c461e2b2bc2ba1dd17c7a7e1e01c54827
Gedion G. Tadesse <yiruubal@gmail.com>
2025-11-11 12:47:14 +0300

    Add tests for GithubOrgClient class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -84,3 +84,27 @@ class TestGetJson(unittest.TestCase):
 
             # Ensure the returned JSON matches test_payload
             self.assertEqual(result, test_payload)
+
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests for GithubOrgClient"""
+
+    @parameterized.expand([
+        ("google", {"login": "google"}),
+        ("abc", {"login": "abc"}),
+    ])
+    @patch("client.get_json")
+    def test_org(self, org_name, expected_payload, mock_get_json):
+        """Test that .org calls get_json once with the right URL and returns its payload"""
+        mock_get_json.return_value = expected_payload
+
+        client = GithubOrgClient(org_name)
+        self.assertEqual(client.org, expected_payload)
+
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+
+if __name__ == "__main__":
+    unittest.main()
+
