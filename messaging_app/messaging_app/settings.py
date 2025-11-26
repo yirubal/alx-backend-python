a883499a31f4175e96dd0298a82656612dadae72
yirubal <yiruubal@gmail.com>
2025-11-26 16:37:55 +0300

    Update Rest_FRAMEWORK  object in settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -50,14 +50,15 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-
-
 }
+
 
 
 from datetime import timedelta
